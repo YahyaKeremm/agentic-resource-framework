4d799f8e6f339f44a525fd6de97d5cf622727f9a
Yahya Kerem <bozaslan24@itu.edu.tr>
2026-03-08 22:55:04 +0300

    #2 done, tested with traffic generator, tests will be included later from main

--- src/environment/environment.py
+++ src/environment/environment.py
@@ -1,10 +1,4 @@
-class Metrics:
-    def __init__(self, N):
-        self.delay = [0]*N
-        self.throughput = [0]*N
-        self.loss = [0]*N
-        self.utilization = [0]*N
-        self.fairness = 0.0
+import numpy as np
 
 class Environment:
     def __init__(self, N=3):
@@ -31,10 +25,8 @@ class Environment:
             th = min(lam[i], mu[i])
             throughput.append(th)
 
-            if (lam[i] < mu[i]):
-                delay.append(1 / (mu[i] - lam[i] + self.eps))
-            else:
-                delay.append(float("inf"))
+            delay_ = 1/ np.maximum(mu[i]-lam[i], self.eps)
+            delay.append(delay_)
 
             loss.append(max(0, u - 1))
 
@@ -53,21 +45,3 @@ class Environment:
         mu = self.compute_service_rates(allocation)
         metrics = self.compute_metrics(lam, mu)
         return metrics
-
-    # def step(self, allocation, demand):
-    #     metrics = Metrics(self.N)
-
-    #     mu = [0]*self.N
-    #     lam = [0]*self.N
-    #     for i in range(self.N):
-    #         mu[i] = self.C_total * allocation[i]
-    #         lam[i] = demand[i]
-
-    #         metrics.utilization[i] = lam[i] / (mu[i] + self.eps)
-    #         metrics.throughput[i] = min(lam[i], mu[i])
-    #         if (lam[i] < mu[i]):
-    #             metrics.delay[i] = 1 / (mu[i] - lam[i] + self.eps)
-    #         else:
-    #             metrics.delay[i] = "OVERLOAD"
-    #         metrics.loss[i] = max(0, metrics.utilization[i] - 1)
-    #     metrics.fairness = (sum(metrics.throughput)**2) / (self.N * sum(x**2 for x in metrics.throughput) + self.eps)
